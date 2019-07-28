014a4ed92aec46092f2ad19ebcd5b4c7cddf608b
Lenny Gingello <lennygingellogd@gmail.com>
2019-07-28 18:05:20 -0400

    Imported Packages and created first person controller

---
+++ PlantProblems/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Camera-Control/Smooth Mouse Look")]
+
+public class PlayerController : MonoBehaviour
+{
+
+    // thank you: https://www.youtube.com/watch?v=blO039OzUZc
+    public float speed = 10.0f;
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+
+    public void Movement()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+
+        }
+    }       
+}
